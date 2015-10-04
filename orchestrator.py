bf5110de82f1143416c5975b17fbd1336e47628b
Kosta <kosta.zabashta@gmail.com>
2015-10-04 19:19:50 -0400

    Started on scraping

--- orchestrator.py
+++ orchestrator.py
@@ -8,7 +8,7 @@ from email_utils.email_extractor import EmailExtractor
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Real Estate'
-DEST_DIR_NAME = 'raw'
+DEST_DIR_NAME = 'test'
 
 def main():
 	fileConfig('logging_config.ini')
@@ -19,11 +19,13 @@ def main():
 
 	for dirName, subdirList, fileList in os.walk(DEST_DIR_NAME):
 	    for fName in fileList:
+	    	if not fName.endswith('.html'):
+	    		continue
 	    	fullPath = os.path.join(dirName, fName)
 	    	with open(fullPath, 'r') as fHandle:
 	    		logger.debug('Procesing %s', fullPath)
 	    		html_doc = fHandle.read().replace('\n', '')
-	    		scaper.scrape(html_doc)
+	    		scraper.scrape(html_doc)
 
 if __name__ == '__main__':
 	main()
--- parser/scraper.py
+++ parser/scraper.py
@@ -1,11 +1,21 @@
 import logging
 
+from bs4 import BeautifulSoup
+
 class Scraper:
 	def __init__(self):
 		self.logger = logging.getLogger(__name__)
 
-	def get_mls_list(html_doc):
+	def get_mls_list(self, html_doc):
+		tables = self.soup.find_all("table", class_="data-list")
+		if len(tables) == 0:
+			return []
 		
+		table = tables[0]
+
+		for row in table.find_all('tr'):
+			print row
 
 	def scrape(self, html_doc):
-		mls_list = get_mls_list()
+		self.soup = BeautifulSoup(html_doc, 'html.parser')
+		mls_list = self.get_mls_list(html_doc)
